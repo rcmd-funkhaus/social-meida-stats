0d2fd5f96cea197ad7337a3183c3c2b67f4ed26a
Azer Abdullaev <azer.abdullaev.berlin@gmail.com>
2020-08-03 22:11:05 +0200

    Added User-Agent header to the curl call

--- instagram.sh
+++ instagram.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-PAYLOAD=$(curl -s "https://www.instagram.com/reactorcoremeltdown/?__a=1")
+PAYLOAD=$(curl -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) QtWebEngine/5.15.0 Chrome/80.0.3987.163 Safari/537.36" -Ls "https://www.instagram.com/reactorcoremeltdown/?__a=1")
 
 echo "DEBUG OUTPUT"
 
